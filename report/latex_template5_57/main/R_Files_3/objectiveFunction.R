5b6fddd8d078b6c326f18951c8551be5c321e6c7
CillianMH <CillianMHN7@gmail.com>
2015-04-29 15:07:57 +0200

    adding report

--- report/latex_template5_57/main/R_Files_3/objectiveFunction.R
+++
@@ -1,7 +0,0 @@
-# Objective function
-# 18-03-2015
-# Killian Martin--Horgassan
-
-objectiveFunction = function(x,n) {
-	output <- log(x) + x^2/2+log(sqrt(2*pi))-log(n)
-}
--- report/latex_template5_57/main/R_Files_3/objectiveFunctionPrime.R
+++
@@ -1,7 +0,0 @@
-# Derivative of the objective function
-# 18-03-2015
-# Killian Martin--Horgassan
-
-objectiveFunctionPrime = function(x) {
-	output <- 1/x + x
-}
